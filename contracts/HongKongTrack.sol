c6e928be8807453223799bd0105f560b2705438a
Cam Jackson <cameroncj86@gmail.com>
2020-09-07 18:29:21 +1000

    Initial commit

--- contracts/HongKongTrack.sol
+++ contracts/HongKongTrack.sol
@@ -1,53 +1,50 @@
 pragma solidity ^0.5.4;
 pragma experimental ABIEncoderV2;
 
-
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    /**
+     * @dev Multiplies two numbers, throws on overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    /**
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    /**
+     * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    /**
+     * @dev Adds two numbers, throws on overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
-
 
 /**
  * @title Ownable
@@ -55,152 +52,198 @@ library SafeMath {
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-  address public owner;
-  address public server;
-  bool public permenantTransfer = false;
-  mapping(address => bool) public whitelist;
+    address public owner;
+    address public server;
+    bool public permenantTransfer = false;
+    mapping(address => bool) public whitelist;
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
 
-
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-    server = msg.sender;
-  }
-
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  modifier onlyWhitelist() {
-    require(whitelist[msg.sender] || msg.sender == owner || msg.sender == server);
-    _;
-  }
-
-  modifier onlyOwnerOrServer() {
-    require(msg.sender == owner || msg.sender == server);
-    _;
-  }
-
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0) && !permenantTransfer);
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-    permenantTransfer = true;
-  }
-
-  function transferServerOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(server, newOwner);
-    server = newOwner;
-  }
-}
-
-
-contract WinnaToken{
-  function deposit(address player, uint betAmount) public payable;
-}
-
-
- contract HongKongTrack is Ownable{
-    mapping(uint => betList) public bets;
-    uint public curBet = 0;
-    uint public processedBet = 0;
-    address winnaAddress;
-    event RaceResult(address player, uint betId, uint[8] outcome, uint winnings);
-
-    struct betList{
-        uint amount;
-        uint[8] wins;
-        uint[8] place;
-        uint[8] show;
-        uint time;
-        address sender;
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        owner = msg.sender;
+        server = msg.sender;
     }
 
-    uint[8] winOdds = [3700000, 5500000, 2600000, 11750000, 17250000, 8750000, 7150000, 6150000];
-    uint[8] placeOdds = [1950000, 2550000, 1250000, 5500000, 7750000, 3050000, 2500000, 2050000];
-    uint[8] showOdds = [1250000, 1700000, 1090000, 2550000, 4000000, 1750000, 1550000, 1350000];
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
+    modifier onlyWhitelist() {
+        require(
+            whitelist[msg.sender] || msg.sender == owner || msg.sender == server
+        );
+        _;
+    }
+
+    modifier onlyOwnerOrServer() {
+        require(msg.sender == owner || msg.sender == server);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0) && !permenantTransfer);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        permenantTransfer = true;
+    }
+
+    function transferServerOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(server, newOwner);
+        server = newOwner;
+    }
+}
+
+contract WinnaToken {
+    function deposit(address player, uint256 betAmount) public payable;
+}
+
+contract HongKongTrack is Ownable {
+    mapping(uint256 => betList) public bets;
+    uint256 public curBet = 0;
+    uint256 public processedBet = 0;
+    address winnaAddress;
+    event RaceResult(
+        address player,
+        uint256 betId,
+        uint256[8] outcome,
+        uint256 winnings
+    );
+
+    struct betList {
+        uint256 amount;
+        uint256[8] wins;
+        uint256[8] place;
+        uint256[8] show;
+        uint256 time;
+        address sender;
+        address referralAddress;
+    }
+
+    uint256[8] winOdds = [
+        3700000,
+        5500000,
+        2600000,
+        11750000,
+        17250000,
+        8750000,
+        7150000,
+        6150000
+    ];
+    uint256[8] placeOdds = [
+        1950000,
+        2550000,
+        1250000,
+        5500000,
+        7750000,
+        3050000,
+        2500000,
+        2050000
+    ];
+    uint256[8] showOdds = [
+        1250000,
+        1700000,
+        1090000,
+        2550000,
+        4000000,
+        1750000,
+        1550000,
+        1350000
+    ];
 
     constructor(address _winnaAddress) public {
         winnaAddress = _winnaAddress;
-      }
-
-
-
-    function setOddsWin(uint[8] memory odds) public onlyOwnerOrServer{
-      winOdds = odds;
     }
 
-    function setOddsPlace(uint[8] memory odds) public onlyOwnerOrServer{
-      placeOdds = odds;
+    function setOddsWin(uint256[8] memory odds) public onlyOwnerOrServer {
+        winOdds = odds;
     }
 
-    function setOddsShow(uint[8] memory odds) public onlyOwnerOrServer{
-      showOdds = odds;
+    function setOddsPlace(uint256[8] memory odds) public onlyOwnerOrServer {
+        placeOdds = odds;
     }
 
-    function placeBet(uint[8] memory wins, uint[8] memory places, uint[8] memory shows) public payable{
-      uint betSum = 0;
-      for(uint x = 0; x < 8; x++){
-        betSum += wins[x];
-        betSum += places[x];
-        betSum += shows[x];
-      }
-      require(betSum <= msg.value, "Insufficien Funds Sent");
-      require(betSum >= 10000000, "Bet Too Low");
-      bets[curBet] = betList(msg.value, wins, places, shows, now, msg.sender);
-
-      curBet++;
+    function setOddsShow(uint256[8] memory odds) public onlyOwnerOrServer {
+        showOdds = odds;
     }
 
-    function concludeRaces(uint[8] memory outcome) public onlyOwnerOrServer{
-      //for(uint x = 0; x < outcome.length; x++){
-        if(processedBet >= curBet){
-          return;
+    function placeBet(
+        uint256[8] memory wins,
+        uint256[8] memory places,
+        uint256[8] memory shows
+    ) public payable {
+        uint256 betSum = 0;
+        for (uint256 x = 0; x < 8; x++) {
+            betSum += wins[x];
+            betSum += places[x];
+            betSum += shows[x];
         }
-        else{
-          uint payout = 0;
-          for(uint y = 0; y < 3; y++){
-            if(bets[processedBet].wins[outcome[y]] > 0 && y < 1){
-              payout += winOdds[outcome[y]] * bets[processedBet].wins[outcome[y]] / 1000000;
+        require(betSum <= msg.value, "Insufficient Funds Sent");
+        require(betSum >= 10000000, "Bet Too Low");
+        bets[curBet] = betList(msg.value, wins, places, shows, now, msg.sender);
+
+        curBet++;
+    }
+
+    function concludeRaces(uint256[8] memory outcome) public onlyOwnerOrServer {
+        //for(uint x = 0; x < outcome.length; x++){
+        if (processedBet >= curBet) {
+            return;
+        } else {
+            uint256 payout = 0;
+            for (uint256 y = 0; y < 3; y++) {
+                if (bets[processedBet].wins[outcome[y]] > 0 && y < 1) {
+                    payout +=
+                        (winOdds[outcome[y]] *
+                            bets[processedBet].wins[outcome[y]]) /
+                        1000000;
+                }
+                if (bets[processedBet].wins[outcome[y]] > 0 && y < 2) {
+                    payout +=
+                        (placeOdds[outcome[y]] *
+                            bets[processedBet].place[outcome[y]]) /
+                        1000000;
+                }
+                if (bets[processedBet].wins[outcome[y]] > 0) {
+                    payout +=
+                        (showOdds[outcome[y]] *
+                            bets[processedBet].show[outcome[y]]) /
+                        1000000;
+                }
             }
-            if(bets[processedBet].wins[outcome[y]] > 0 && y < 2){
-              payout += placeOdds[outcome[y]] * bets[processedBet].place[outcome[y]] / 1000000;
+            make_payable(bets[processedBet].sender).transfer(payout);
+            emit RaceResult(
+                bets[processedBet].sender,
+                processedBet,
+                outcome,
+                payout
+            );
+            if (payout < bets[processedBet].amount) {
+                WinnaToken(winnaAddress).deposit.value(
+                    bets[processedBet].amount - payout
+                )(bets[processedBet].sender, bets[processedBet].amount);
             }
-            if(bets[processedBet].wins[outcome[y]] > 0){
-              payout += showOdds[outcome[y]] * bets[processedBet].show[outcome[y]] / 1000000;
-            }
-          }
-          make_payable(bets[processedBet].sender).transfer(payout);
-          emit RaceResult(bets[processedBet].sender, processedBet, outcome, payout);
-          if(payout < bets[processedBet].amount){
-            WinnaToken(winnaAddress).deposit.value(bets[processedBet].amount - payout)(bets[processedBet].sender, bets[processedBet].amount);
-          }
-          processedBet++;
+            processedBet++;
         }
-      //}
+        //}
     }
 
     function make_payable(address x) internal pure returns (address payable) {
-     return address(uint160(x));
-      }
-
-
-
-
-
- }
+        return address(uint160(x));
+    }
+}
--- contracts/WinnaToken.sol
+++ contracts/WinnaToken.sol
@@ -271,48 +271,48 @@ contract StandardToken is ERC20, BasicToken {
 
 }
 
-/**
- * @title Mintable token
- * @dev Simple ERC20 Token example, with mintable token creation
- * @dev Issue: * https://github.com/OpenZeppelin/zeppelin-solidity/issues/120
- * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
- */
-contract MintableToken is StandardToken, Ownable {
-  event Mint(address indexed to, uint256 amount);
-  event MintFinished();
+// /**
+//  * @title Mintable token
+//  * @dev Simple ERC20 Token example, with mintable token creation
+//  * @dev Issue: * https://github.com/OpenZeppelin/zeppelin-solidity/issues/120
+//  * Based on code by TokenMarketNet: https://github.com/TokenMarketNet/ico/blob/master/contracts/MintableToken.sol
+//  */
+// contract MintableToken is StandardToken, Ownable {
+//   event Mint(address indexed to, uint256 amount);
+//   event MintFinished();
 
-  bool public mintingFinished = false;
+//   bool public mintingFinished = false;
 
 
-  modifier canMint() {
-    require(!mintingFinished);
-    _;
-  }
+//   modifier canMint() {
+//     require(!mintingFinished);
+//     _;
+//   }
 
-  /**
-   * @dev Function to mint tokens
-   * @param _to The address that will receive the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) onlyWhitelist public returns (bool) {
-    totalSupply_ = totalSupply_.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    emit Mint(_to, _amount);
-    emit Transfer(address(0), _to, _amount);
-    return true;
-  }
+//   /**
+//    * @dev Function to mint tokens
+//    * @param _to The address that will receive the minted tokens.
+//    * @param _amount The amount of tokens to mint.
+//    * @return A boolean that indicates if the operation was successful.
+//    */
+//   function mint(address _to, uint256 _amount) onlyWhitelist public returns (bool) {
+//     totalSupply_ = totalSupply_.add(_amount);
+//     balances[_to] = balances[_to].add(_amount);
+//     emit Mint(_to, _amount);
+//     emit Transfer(address(0), _to, _amount);
+//     return true;
+//   }
 
-  /**
-   * @dev Function to stop minting new tokens.
-   * @return True if the operation was successful.
-   */
-  function finishMinting() onlyOwner canMint public returns (bool) {
-    mintingFinished = true;
-    emit MintFinished();
-    return true;
-  }
-}
+//   /**
+//    * @dev Function to stop minting new tokens.
+//    * @return True if the operation was successful.
+//    */
+//   function finishMinting() onlyOwner canMint public returns (bool) {
+//     mintingFinished = true;
+//     emit MintFinished();
+//     return true;
+//   }
+// }
 
 contract Erc20Token is MintableToken {
 
